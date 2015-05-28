97bcf7d41b9dc25344111b4dabe7967cfdf744d5
Matt Hoiland <thematthoiland@gmail.com>
2015-05-28 11:04:17 -0700

    Return name of color

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
  * Module dependencies
  */
 var express     = require('express');
-var color       = require('tinycolor2');
+var tinycolor   = require('tinycolor2');
 var slack       = require('node-slack');
-var config      = require('./config');
+var config      = require('./lib/config');
+var utils       = require('./lib/utils');
 
 
 /**
@@ -19,7 +20,21 @@ var port        = process.env.PORT || 3000;
  * Routes
  */
 app.get('/', function(req, res, next){
-    return res.send('working');
+    var color = req.query.color || '000000';
+    var name = utils.getColorName(color);
+
+    var triad = new tinycolor(color);
+    triad = triad.triad().map(function(t){ return t.toHexString(); });
+
+    var message = '<div style="display:inline-block;height:20px;width:20px;background:'+triad[0]+'"></div>';
+    message += ' <span style="line-height:20px">';
+    message += name + '</span>';
+    message += "<div></div>";
+    message += '<div style="display:inline-block;height:20px;width:20px;background:'+triad[0]+'"></div>';
+    message += '<div style="display:inline-block;height:20px;width:20px;background:'+triad[1]+'"></div>';
+    message += '<div style="display:inline-block;height:20px;width:20px;background:'+triad[2]+'"></div>';
+
+    return res.send(message);
 });
 
 
